1fed06dfb4c2e89980e3bccc013f9db1459f27d9
blue4570 <butcheraaron@icloud.com>
2020-06-22 22:53:44 -0500

    Updated in progress to allow program to handle any csv without using POJO

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,11 +2,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        Writer writer = new Writer("$", "ID", 1);
-        try {
+        Writer writer = new Writer("$", "ID", 3);
             writer.write();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 }
--- src/main/java/Writer.java
+++ src/main/java/Writer.java
@@ -1,3 +1,5 @@
+import javafx.beans.binding.StringBinding;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
@@ -5,19 +7,15 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.regex.Matcher;
+import java.util.UUID;
 import java.util.regex.Pattern;
 
 public class Writer {
 
     private final File csvFile;
     private final String delimiter;
-    private final String formattedDelimiter;
     private final String newHeader;
     private final int newHeaderPosition;
     static final String NEW_FILE_LOCATION_AND_NAME = "/Users/Aaron/IdeaProjects/CsvAppender/src/main/resources/csv/newCsv.txt";
@@ -25,19 +23,19 @@ public class Writer {
     public Writer(String delimiter, String newheader, int newHeaderPosition) {
         this.csvFile = new File(getClass().getClassLoader().getResource("csv/TestCsv.txt").getFile());
         this.newHeader = newheader;
-        this.newHeaderPosition = newHeaderPosition;
+        this.newHeaderPosition = newHeaderPosition - 1;
         this.delimiter = delimiter;
-        this.formattedDelimiter = getDelimiter(delimiter);
     }
 
-    public void write() throws IOException {
-       // FileWriter writer = getFileWriter();
+    public void write() {
         Map<String, String[]> data = getOriginalFileData(csvFile);
         String[] origHeaders = data.get("HEADERS");
-      //  String[] allRows = data.get("ROWS");
+        String[] allRows = data.get("ROWS");
         int totalNumberOfColumns = origHeaders.length + 1;
+
         try(FileWriter writer = new FileWriter(NEW_FILE_LOCATION_AND_NAME)) {
             writeHeaders(writer, origHeaders, this.newHeader, this.newHeaderPosition, totalNumberOfColumns);
+            writeRows(writer, allRows, this.newHeaderPosition, totalNumberOfColumns);
         } catch (IOException e) {
             System.out.println("Error writing to new file");
             e.printStackTrace();
@@ -46,27 +44,65 @@ public class Writer {
 
     }
 
-    private void writeHeaders(FileWriter writer, String[] origHeaders,String newHeader, int newHeaderPosition, int totalNumOfColumns) throws IOException {
+    private void writeHeaders(FileWriter writer, String[] origHeaders,String newHeader, int positionToAdd, int totalNumOfColumns) throws IOException {
         String[] newHeaders = new String[totalNumOfColumns];
-        int posToAdd = newHeaderPosition - 1;
-        newHeaders[posToAdd] = newHeader;
+        newHeaders[positionToAdd] = newHeader;
         int pos = 0;
 
         for(int i = 0; i < origHeaders.length; i++) {
-            if(posToAdd == i) {
-                pos = posToAdd + 1;
+            if(positionToAdd == i) {
+                pos = positionToAdd + 1;
             }
             newHeaders[pos] = origHeaders[i];
             pos++;
         }
 
-        for (String s: newHeaders) {
-            writer.append(s).append(delimiter);
+        for(int i = 0; i < newHeaders.length ; i++) {
+            if(i < (newHeaders.length - 1)) {
+                writer.append(newHeaders[i]).append(this.delimiter);
+            } else {
+                writer.append(newHeaders[i]);
+            }
         }
         writer.append("\n");
     }
 
-    private void writeRows() {
+    //TODO Refactor to add new ID column
+    private void writeRows(FileWriter writer, String[] originalRows, int positionToAdd, int totalNumOfColumns) throws IOException {
+
+        String[] newRows = new String[originalRows.length];
+        String[] newRowData = new String[totalNumOfColumns];
+        for(int i = 0 ; i < originalRows.length ; i++) {
+            int pos = 0;
+            newRowData[positionToAdd] = String.valueOf(UUID.randomUUID());
+            String[] origColumnRowData = originalRows[i].split(getFormattedDelimiter(this.delimiter));
+            StringBuilder sb = new StringBuilder();
+
+            for(int r = 0 ; r < origColumnRowData.length; r++) {
+                if(positionToAdd == r) {
+                    pos = positionToAdd + 1;
+                }
+                newRowData[pos] = origColumnRowData[r];
+                pos++;
+            }
+
+            for(int d = 0; d < newRowData.length; d++) {
+                if(d < totalNumOfColumns -1) {
+                    sb.append(newRowData[d]).append(this.delimiter);
+                } else {
+                    sb.append(newRowData[d]);
+                }
+                newRows[i] = sb.toString();
+            }
+        }
+
+            for(int i = 0 ; i < newRows.length ; i++) {
+                if(i < (newRows.length -1)) {
+                    writer.append(newRows[i]).append("\n");
+                } else {
+                    writer.append(newRows[i]);
+                }
+            }
 
     }
 
@@ -78,10 +114,16 @@ public class Writer {
         ) {
             if(br.ready()) {
                 csv = br.lines().toArray(String[]::new);
-                String header = csv[0];
-                String[] headers = header.split(this.formattedDelimiter);
+                String[] rows = new String[csv.length-1];
+                String[] headers = csv[0].split(getFormattedDelimiter(this.delimiter));
+                for(int i = 0; i < csv.length; i++) {
+                    if(i > 0) {
+                        int insertPosition = i - 1;
+                        rows[insertPosition] = csv[i];
+                    }
+                }
                 dataMap.put("HEADERS", headers);
-                //     dataMap.put("ROWS", csv);
+                dataMap.put("ROWS", rows);
             } else {
                 throw new IOException();
             }
@@ -91,13 +133,7 @@ public class Writer {
           return dataMap;
     }
 
-    //private final Function<String, String[]> getData = (line) -> line.split("\n");
-
-    private FileWriter getFileWriter() throws IOException {
-        return new FileWriter(NEW_FILE_LOCATION_AND_NAME);
-    }
-
-    private String getDelimiter(String delimiter) {
+    private String getFormattedDelimiter(String delimiter) {
         return isAlphaNumeric(delimiter) ? delimiter : "\\" + delimiter;
     }
 
